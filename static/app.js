b1decec9be103c608d2b25cecab324c16b741c2b
TajraT <tajra.tasic.24@size.ba>
2025-05-22 08:31:34 +0200

    Slike i opis

--- static/app.js
+++ static/app.js
@@ -1,32 +1,291 @@
 const menuData = {
   dorucak: [
     {
-      name: "Omlet sa sirom",
-      description: "Jaja, sir, začini",
-      price: "6 KM",
-      image: "/static/images/1.jpg"
+      name: "Pura ",
+      description: "Kuhana kukuruzna kaša",
+      price: "7 KM",
+      image: "/static/images/1.avif"
     },
     {
-      name: "Pohovani hljeb",
-      description: "Hljeb, jaja, sir",
-      price: "5 KM",
-      image: "nesto"
-    }
+      name: "Omlet sa sirom",
+      description: "Jaja sa sirom",
+      price: "7 KM",
+      image: "/static/images/2.avif"
+    },
+    {
+      name: "Omlet sa povrćem",
+      description: "Jaja sa dinstanim povrćem",
+      price: "7 KM",
+      image: "/static/images/3.avif"
+    },
+    {
+      name: "Omlet sa suhim mesom i sirom",
+      description: "Jaja, suho meso, sir",
+      price: "8 KM",
+      image: "/static/images/4.avif"
+    },
+    {
+      name: "Uštipci sa pavlakom i sirom",
+      description: "Pržene kuglice sa sirom",
+      price: "7 KM",
+      image: "/static/images/5.avif"
+    },
+    {
+      name: "Uštipci sa suhim mesom, pavlakom i sirom",
+      description: "Pržene kuglice sa suhim mesom, sirom i pavlakom",
+      price: "9 KM",
+      image: "/static/images/6.avif"
+    },
+    {
+      name: "Pohovani sir",
+      description: "Prženi sir u hrskavoj kori",
+      price: "10 KM",
+      image: "/static/images/7.avif"
+    },
+    {
+      name: "Author doručak",
+      description: "Jaja, sir, šunka, kobasice, suho meso, uštipci",
+      price: "10 KM",
+      image: "/static/images/8.avif"
+    },
+    {
+      name: "Author doručak za dvoje",
+      description: "Sir, šunka, kobasice, suho meso, uštipci",
+      price: "18 KM",
+      image: "/static/images/9.avif"
+    },
   ],
 
-  salate: [
+  prilog: [
     {
-      name: "Omlet sa sirom",
-      description: "Jaja, sir, začini",
-      price: "6 KM",
-      image: "/static/images/1.jpg"
+      name: "Pecivo ",
+      description: "Pečeno tijesto",
+      price: "1.50 KM",
+      image: "/static/images/14.avif"
     },
     {
-      name: "Pohovani hljeb",
-      description: "Hljeb, jaja, sir",
+      name: "Pomfrit",
+      description: "Prženi štapići od krompira",
+      price: "4 KM",
+      image: "/static/images/10.avif"
+    },
+    {
+      name: "Pekarski krompir",
+      description: "Pečeni krompir sa začinima",
+      price: "4.50 KM",
+      image: "/static/images/11.avif"
+    },
+    {
+      name: "Grilovano povrće",
+      description: "Povrće pečeno na roštilju",
       price: "5 KM",
-      image: "https://source.unsplash.com/400x300/?toast"
-    }
+      image: "/static/images/12.avif"
+    },
+    {
+      name: "Riža",
+      description: "Skuhana zrna žitarice",
+      price: "4 KM",
+      image: "/static/images/13.avif"
+    },
+
+  ],
+  salate: [
+    {
+      name: "Sezonska salata ",
+      description: "Svježe povrće iz sezone",
+      price: "4 KM",
+      image: "/static/images/15.avifs"
+    },
+    {
+      name: "Pileća obrok salata",
+      description: "Salata s piletinom, povrćem",
+      price: "10 KM",
+      image: "/static/images/16.avif"
+    },
+    {
+      name: "Tuna obrok salata",
+      description: "Salata s tunom, povrćem",
+      price: "10 KM",
+      image: "/static/images/17.avif"
+    },
+    
+  ],
+  piletina: [
+    {
+      name: "Wrap sa piletinom i pomfritom ",
+      description: "Tortilja, piletina, pomfrit",
+      price: "8 KM",
+      image: "/static/images/18.avif"
+    },
+    {
+      name: "Pileća maslanica",
+      description: "Piletina u kremastom sosu",
+      price: "12 KM",
+      image: "/static/images/19.avif"
+    },
+    {
+      name: "Pileći ražnjići",
+      description: "Uštipci, slata, pomfrit, piletina",
+      price: "12 KM",
+      image: "/static/images/20.avif"
+    },
+    {
+      name: "Chicken fingers sa susamom",
+      description: "Pohovana piletina i susam",
+      price: "14 KM",
+      image: "/static/images/21.avif"
+    },
+    {
+      name: "Punjena piletina sa sirom",
+      description: "Pečena piletina punjena sirom",
+      price: "14 KM",
+      image: "/static/images/22.avif"
+    },
+    {
+      name: "Piletina u curry sosu",
+      description: "Pečena piletina, alata, pomfrit",
+      price: "14 KM",
+      image: "/static/images/23.avif"
+    },
+    {
+      name: "Piletina 4 sira",
+      description: "Prekriveno sa 4 vrste sira",
+      price: "14 KM",
+      image: "/static/images/24.jpg"
+    },
+    {
+      name: "Piletina u sweet chilli sosu",
+      description: "Slatko-začinjeni sos",
+      price: "14 KM",
+      image: "/static/images/25.avif"
+    },
+    {
+      name: "Hot cilli piletina",
+      description: "Ljuti sos",
+      price: "14 KM",
+      image: "/static/images/26.avif"
+    },
+    {
+      name: "Piletina u sosu od gljiva",
+      description: "Pomfrit, piletina, vrhnje i gljive",
+      price: "14 KM",
+      image: "/static/images/27.avif"
+    },
+    {
+      name: "Piletina sa kikirikijem",
+      description: "Aromatični kikiriki",
+      price: "14 KM",
+      image: "/static/images/28.avif"
+    },
+    {
+      name: "Piletina na bečki način",
+      description: "Pohovanapiletina, hrskava i zlatna",
+      price: "14 KM",
+      image: "/static/images/29.avif"
+    },
+
+  ],
+  teletina: [
+    {
+      name: "Teleća natur šnicla ",
+      description: "Pečena i sočna",
+      price: "23KM",
+      image: "/static/images/30.avif"
+    },
+    {
+      name: "Teleća bečka šnicla",
+      description: "Pohovana i hrskava",
+      price: "25 KM",
+      image: "/static/images/31.avif"
+    },
+    {
+      name: "Teleći medaljoni u sosu od gljiva",
+      description: "Mali, debeli, okrugli komadi mesa",
+      price: "28 KM",
+      image: "/static/images/32.avif"
+    },
+    {
+      name: "Teletina pohovana u parmezanu",
+      description: "Pohovana teletina",
+      price: "28 KM",
+      image: "/static/images/33.avif"
+    },
+
+  ],
+ 
+  junetina: [
+    {
+      name: "Biftek grill ",
+      description: "Sočan, pečeni komad mesa",
+      price: "38 KM",
+      image: "/static/images/34.avif"
+    },
+    {
+      name: "Biftek u sosu zelenog papra",
+      description: "Pikantni sos papra",
+      price: "40 KM",
+      image: "/static/images/35.avif"
+    },
+    {
+      name: "Biftek u sosu od gljiva",
+      description: "Kremasti gljiva sos",
+      price: "40 KM",
+      image: "/static/images/36.avif"
+    },
+
+  ],
+  pasta: [
+    {
+      name: "Mac & Cheese ",
+      description: "Tjestenina i kremasti sir",
+      price: "10 KM",
+      image: "/static/images/37.avif"
+    },
+    {
+      name: "Penne sa piletinom i gljivama",
+      description: "Aromatični sos od gljiva",
+      price: "11 KM",
+      image: "/static/images/38.avif"
+    },
+    {
+      name: "Penne 4 vrste sira",
+      description: "4 topljena sira",
+      price: "11 KM",
+      image: "/static/images/39.avif"
+    },
+    {
+      name: "Špagete bolonjez",
+      description: "Mesni, paradajz sosu",
+      price: "12 KM",
+      image: "/static/images/40.avif"
+    },
+    {
+      name: "Tagliatelle sa lososom",
+      description: "Široke tjestenine s lososom u sosu",
+      price: "15 KM",
+      image: "/static/images/41.avif"
+    },
+
+  ],
+  corbe: [
+    {
+      name: "Teleća čorba ",
+      description: "Gusta supa od telećeg mesa",
+      price: "7 KM",
+      image: "/static/images/42.avif"
+    },
+    {
+      name: "Begova čorba",
+      description: "Bogata mesom i povrćem",
+      price: "6 KM",
+      image: "/static/images/43.avif"
+    },
+    {
+      name: "Krem supa od gljiva",
+      description: "Gusta, kremasta supa s aromatičnim gljivama.",
+      price: "6 KM",
+      image: "/static/images/44.avif"
+    },
 
   ],
   // Dodaj i ostale kategorije kao u prethodnom kodu
