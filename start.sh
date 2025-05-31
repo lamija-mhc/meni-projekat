71378eccd5d5b21df33723ad9d626ba2f60b1348
mehic-l <lamijaamehic@gmail.com>
2025-05-31 20:26:39 +0200

    Dodajem start.sh iz grane lamija

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python3 app.py
+
+echo "🔧 Buildam Docker image: meni-projekat"
+docker build -t meni-projekat .
+
+echo "🚀 Pokrećem Docker kontejner..."
+docker run -d -p 5000:5000 meni-projekat
+
+echo "🌐 Aplikacija je sada dostupna na http://localhost:5000"
