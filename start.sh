69431b819189ef619a7cdf51e57f4cb0e146dfad
mehic-l <lamijaamehic@gmail.com>
2025-05-31 20:33:45 +0200

    Start.sh bugovi

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ echo "🔧 Buildam Docker image: meni-projekat"
 docker build -t meni-projekat .
 
 echo "🚀 Pokrećem Docker kontejner..."
-docker run -d -p 5000:5000 meni-projekat
+docker run -d -p 10000:10000 meni-projekat
 
-echo "🌐 Aplikacija je sada dostupna na http://localhost:5000"
+echo "🌐 Aplikacija je sada dostupna na http://localhost:10000"
